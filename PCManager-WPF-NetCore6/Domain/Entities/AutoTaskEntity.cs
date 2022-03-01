1a81562dae3970bf75b128c005a42078483d7a43
TMLShared <97077711+TMLShared@users.noreply.github.com>
2022-03-01 16:39:48 +0700

    add feature auto task

---
+++ PCManager-WPF-NetCore6/Domain/Entities/AutoTaskEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCManager_WPF_NetCore6.Domain.Entities
+{
+    public class AutoTaskEntity
+    {
+        public enum Types { News, SystemIssue }
+        public Types Type { get; set; }
+        public States State { get; set; }
+        public string Content { get; set; }
+    }
+}
