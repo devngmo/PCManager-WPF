6f44878701ce2a8a8064a08125a675f29e126fd0
TMLShared <97077711+TMLShared@users.noreply.github.com>
2022-03-01 16:40:49 +0700

    use IntentAction to process user action when user click on a Note

---
+++ PCManager-WPF-NetCore6/AppLayer/ViewModels/IntentAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCManager_WPF_NetCore6.AppLayer.ViewModels
+{
+    public class IntentAction
+    {
+        public enum Types { View, Delete, Add, Edit }
+        public Types Type { get; set; }
+    }
+}
